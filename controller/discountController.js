95d9deb61f5c756da3d17051f5d3f8e4ce70bdc2
Avi Tamrakar <abis39112@gmail.com>
2025-08-20 16:29:11 +0530

    discount controller updated

--- controller/discountController.js
+++ controller/discountController.js
@@ -1,6 +1,17 @@
 const Discount = require("../models/Discount"); // Import the Discount model
 const Coupon = require("../models/Coupon"); // Assuming you have a Coupon model
 
+// Get all discounts
+exports.getDiscounts = async (req, res) => {
+  try {
+    const discounts = await Discount.find({}).sort({ createdAt: -1 });
+    res.status(200).json(discounts);
+  } catch (err) {
+    console.error('Error in getDiscounts:', err);
+    res.status(500).json({ error: 'Error fetching discounts' });
+  }
+};
+
 // Create new discount
 exports.createDiscount = async (req, res) => {
   try {
