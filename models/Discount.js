fda302e765584408be53a5b435b3162dafc552c8
Avi Tamrakar <abis39112@gmail.com>
2025-08-20 16:55:52 +0530

    discount model updated

--- models/Discount.js
+++ models/Discount.js
@@ -1,40 +1,41 @@
 const mongoose = require('mongoose');
 
 const discountSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String
-    },
-    discountPercentage: {
-        type: Number,
-        required: true
-    },
-    startDate: {
-        type: Date,
-        required: true
-    },
-    endDate: {
-        type: Date,
-        required: true
-    },
-    startTime: {
-        type: String,
-        required: true
-    },
-    endTime: {
-        type: String,
-        required: true
-    },
-    isActive: {
-        type: Boolean,
-        default: true
-    }
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+  startTime: {
+    type: String,
+    required: true, // e.g., "14:00"
+    match: /^([0-1]\d|2[0-3]):([0-5]\d)$/,
+  },
+  endTime: {
+    type: String,
+    required: true, // e.g., "18:00"
+    match: /^([0-1]\d|2[0-3]):([0-5]\d)$/,
+  },
+  discountType: {
+    type: String,
+    enum: ['percent', 'fixed'],
+    required: true,
+  },
+  discountValue: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  store: {
+    type: Number,
+    required: true,
+  },
+
 }, {
-    timestamps: true
+  timestamps: true
 });
 
-const Discount = mongoose.model('Discount', discountSchema);
-module.exports = Discount;
+module.exports = mongoose.model('Discount', discountSchema);
